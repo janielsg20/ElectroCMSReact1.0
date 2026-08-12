a34b26e04e77fbaf6dd4e6fa90c8197b0d9d7913
janielsg20 <72001897+janielsg20@users.noreply.github.com>
2026-08-11 20:50:49 -0700

    export ElectroCMS form controls

--- src/editor-ui/primitives/index.ts
+++ src/editor-ui/primitives/index.ts
@@ -1,3 +1,22 @@
 export { Button, type ButtonProps } from './Button'
 export { Icon, type IconName, type IconProps } from './Icon'
 export { TextField, type TextFieldProps } from './TextField'
+export {
+  Checkbox,
+  ControlGroup,
+  ControlInput,
+  RadioGroup,
+  Select,
+  Switch,
+  TextArea,
+  type CheckboxProps,
+  type ControlGroupProps,
+  type ControlInputProps,
+  type ControlSize,
+  type RadioGroupProps,
+  type RadioOption,
+  type SelectOption,
+  type SelectProps,
+  type SwitchProps,
+  type TextAreaProps,
+} from './FormControls'
