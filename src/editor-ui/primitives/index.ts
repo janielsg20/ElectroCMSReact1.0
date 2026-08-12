744ac126974b6164ebb47df4ed8b9a189235d866
janielsg20 <72001897+janielsg20@users.noreply.github.com>
2026-08-12 08:13:44 -0700

    feat(ux): export contextual help primitive

--- src/editor-ui/primitives/index.ts
+++ src/editor-ui/primitives/index.ts
@@ -1,3 +1,4 @@
 export { Button, type ButtonProps } from './Button'
+export { HelpTip, type HelpTipProps } from './HelpTip'
 export { Icon, type IconName, type IconProps } from './Icon'
 export { TextField, type TextFieldProps } from './TextField'
