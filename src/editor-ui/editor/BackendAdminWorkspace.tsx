17b507befb0482f49900ea279b29de6a4f2ae2c3
janielsg20 <72001897+janielsg20@users.noreply.github.com>
2026-08-13 09:31:08 -0700

    feat: integrate RBAC manager in admin workspace

--- src/editor-ui/editor/BackendAdminWorkspace.tsx
+++ src/editor-ui/editor/BackendAdminWorkspace.tsx
@@ -2,6 +2,7 @@ import { adminShellForDocument } from '../../domain/project/backend-shell-engine
 import { AdminCrudViewsManager } from './AdminCrudViewsManager'
 import { BackendShellManager } from './BackendShellManager'
 import { useEditorProject, useEditorProjectStructure } from './editor-project-context'
+import { RolePermissionManager } from './RolePermissionManager'
 
 export function BackendAdminWorkspace() {
   const session = useEditorProject()
@@ -12,6 +13,7 @@ export function BackendAdminWorkspace() {
     <div className="grid gap-2">
       <BackendShellManager />
       {shell ? <div className="px-2 pb-2 lg:px-3 lg:pb-3"><AdminCrudViewsManager screenId={shell.screen.id} /></div> : null}
+      <div className="px-2 pb-2 lg:px-3 lg:pb-3"><RolePermissionManager /></div>
     </div>
   )
 }
